8c2e8a03e1de34d843996ea285bc5bb8546d6bd2
VladislavVassilyev6627 <n01436627@humbermail.ca>
2022-02-23 18:11:16 -0500

    Initial commit Vladislav Vassilyev

---
+++ vladislavTestCC/src/main/java/vladislav/vassilyev/n01436627/c/MainActivity.java
@@ -0,0 +1,14 @@
+package vladislav.vassilyev.n01436627.c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
